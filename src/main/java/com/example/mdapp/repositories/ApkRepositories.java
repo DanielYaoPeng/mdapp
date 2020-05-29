4055f2980775d6273118f1d8efbc212ca2d91808
yaopeng <down953@163.com>
2020-05-29 17:50:31 +0800

    feat:批量获取应用

--- src/main/java/com/example/mdapp/repositories/ApkRepositories.java
+++ src/main/java/com/example/mdapp/repositories/ApkRepositories.java
@@ -4,7 +4,10 @@ import com.example.mdapp.entity.Apk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
@@ -18,5 +21,11 @@ public class ApkRepositories {
     public Apk get(String  apkId){
 
         return mongoTemplate.findOne(query(where("apkid").is(apkId)),Apk.class);
+    }
+
+    public List<Apk> gets(List<String> ids){
+        Query query=Query.query(where("apkid").in(ids));
+        List<Apk> result=  mongoTemplate.find(query,Apk.class);
+        return result;
     }
 }
