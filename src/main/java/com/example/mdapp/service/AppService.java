06a4acde2953de0d5d199ee3485debe5745cc3f5
yaopeng <down953@163.com>
2020-10-30 14:18:45 +0800

    add appservice Impl

--- src/main/java/com/example/mdapp/service/AppService.java
+++ src/main/java/com/example/mdapp/service/AppService.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.mdapp.entity.Apk;
-import com.example.mdapp.entity.ApkStatusEnum;
+
 import com.example.mdapp.repositories.AppRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import org.springframework.data.mongodb.core.query.Query;
-import static org.springframework.data.mongodb.core.query.Query.query;
+
 
 public class AppService {
 
@@ -20,7 +18,7 @@ public class AppService {
     public List<Apk> Get(String id){
         List<String> ids=new ArrayList<String>();
         ids.add(id);
-        Query query = query(where("status").is(ApkStatusEnum.Enable.getValue()).and("apkid").in(ids));
+       
         var result= _appRepository.findAll();
         return result;
     }
